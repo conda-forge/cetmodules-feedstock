ef273e482d33e59d78eb8699692af9551e6af001
Jacob Calcutt <jcalcutt@bnl.gov>
2026-06-22 10:17:52 -0700

    Standardized the install locations like later versions

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,7 +1,14 @@
 #!/bin/bash -e
+# cetmodules_CONFIG_OUTPUT_ROOT_DIR: in 3.24.01 the generated cetmodulesConfig.cmake
+# defaults under the (arch-dependent) library dir -> lib/cetmodules/cmake. cetmodules
+# 4.x defaults it to the arch-independent data dir -> share/cetmodules/cmake. Set it
+# explicitly here so this noarch package matches the 4.x layout.
 cmake -S ${SRC_DIR} -B build -G Ninja \
     -DCMAKE_INSTALL_PREFIX=${PREFIX} \
     -DCMAKE_INSTALL_LIBEXECDIR=libexec/cetmodules \
+    -Dcetmodules_CONFIG_OUTPUT_ROOT_DIR=share \
+    -Dcetmodules_ETC_DIR=share/cetmodules/etc \
+    -Dcetmodules_TOOLS_DIR=share/cetmodules/tools \
     -DBUILD_TESTING=OFF
 cmake --build build --parallel ${CPU_COUNT} --target install
 
